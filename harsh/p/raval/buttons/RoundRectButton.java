ae77c0c618bdc6d8c60b107bbfb809e2ec014fd6
Harsh Raval <harryjoy_777@yahoo.com>
2012-01-22 18:48:24 +0530

    Package Change

---
+++ harsh/p/raval/buttons/RoundRectButton.java
@@ -0,0 +1,99 @@
+// Copyright (c) 2011, Harsh P. Raval
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, 
+// with or without modification, are permitted provided that the 
+// following conditions are met:
+//
+// Redistributions of source code must retain the above copyright notice, 
+// this list of conditions and the following disclaimer.
+// 
+// Redistributions in binary form must reproduce the above copyright notice, 
+// this list of conditions and the following disclaimer in the documentation 
+// and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
+// OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+// THE POSSIBILITY OF SUCH DAMAGE.
+package harsh.p.raval.buttons;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Shape;
+import java.awt.geom.RoundRectangle2D;
+
+import javax.swing.Action;
+import javax.swing.Icon;
+import javax.swing.JButton;
+
+/**
+ * To make round rectangle shaped button. 
+ * @author harsh
+ */
+public class RoundRectButton extends JButton {
+	private static final long serialVersionUID = -5264781391103062590L;
+	private Shape shape;
+	public RoundRectButton(String label) {
+		super(label);
+		Dimension size = getPreferredSize();
+		size.width = size.height = Math.max(size.width, size.height);
+		setPreferredSize(size);
+		setContentAreaFilled(false);
+	}
+	public RoundRectButton(String label, Icon icon) {
+		super(label, icon);
+		Dimension size = getPreferredSize();
+		size.width = size.height = Math.max(size.width, size.height);
+		setPreferredSize(size);
+		setContentAreaFilled(false);
+	}
+	public RoundRectButton(Icon icon) {
+		super(icon);
+		Dimension size = getPreferredSize();
+		size.width = size.height = Math.max(size.width, size.height);
+		setPreferredSize(size);
+		setContentAreaFilled(false);
+	}
+	public RoundRectButton(Action action) {
+		super(action);
+		Dimension size = getPreferredSize();
+		size.width = size.height = Math.max(size.width, size.height);
+		setPreferredSize(size);
+		setContentAreaFilled(false);
+	}
+	protected void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		if (getModel().isArmed()) {
+			g2.setColor(Color.lightGray);
+		} else {
+			g2.setColor(getBackground());
+		}
+		g2.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
+		super.paintComponent(g2);
+	}
+	protected void paintBorder(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2.setColor(getForeground());
+		g2.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
+	}
+
+	public boolean contains(int x, int y) {
+		if (shape == null || !shape.getBounds().equals(getBounds())) {
+			shape = new RoundRectangle2D.Float(0, 0, getWidth() - 1,
+					getHeight() - 1, 15, 15);
+		}
+		return shape.contains(x, y);
+	}
+}
